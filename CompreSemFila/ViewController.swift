928471e5ae973a8b370048dc00f18a6797c3a2e7
Adriano Braga <sbragaadriano@gmail.com>
2017-04-07 00:38:26 -0300

    update final project | production

--- CompreSemFila/ViewController.swift
+++ CompreSemFila/ViewController.swift
@@ -10,23 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    
+    @IBOutlet weak var navigationTitle: UINavigationItem!
     @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url = URL(string: "https://www.compresemfila.com.br/pageinicial-login")
+        let url = URL(string: "https://www.compresemfila.com.br")
         webView.loadRequest(URLRequest(url: url!))
     }
+
+    @IBAction func backButton(_: UIButton) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
     
-    // Botao voltar comentado para Luiz
-//    @IBAction func backButton(sender: AnyObject!) {
-//        webView.goBack()
-//    }
-    // Botão avançar comentado para Luiz
-//    @IBAction func forwardButton(sender: AnyObject!) {
-//        webView.goForward()
-//    }
+    @IBAction func forwardButton(_: UIButton) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
